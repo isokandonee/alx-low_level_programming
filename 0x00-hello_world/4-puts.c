8213a1ff73e1747908a1f53ee21c8919e53dbafe
Isokan Ogunniyi <isokandone@gmail.com>
2022-12-18 12:14:07 -0800

    d

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
-
-int main(void) {
+/**
+ * main - Prints quote
+ * Return: zero on success
+ */
+int main(void}
+{
   puts("\"Programming is like building a multilingual puzzle");
   return 0;
 }

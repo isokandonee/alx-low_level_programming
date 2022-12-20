74f046e5a8ff0069b3bd103dcaabc87dddd25579
Isokan Ogunniyi <isokandone@gmail.com>
2022-12-20 07:22:19 -0800

    betty compliance for 0-7

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -8,10 +8,9 @@
  */
 void puts2(char *str)
 {
-  int string;
-
-  for (string = 0; str[string] != '\0'; string++)
-    if (string % 2 == 0)
-      _putchar(str[string]);
-  _putchar('\n');
+int string;
+for (string = 0; str[string] != '\0'; string++)
+if (string % 2 == 0)
+_putchar(str[string]);
+_putchar('\n');
 }

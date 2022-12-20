21f9e14fee21900b77692f484683c5e458e959be
Isokan Ogunniyi <isokandone@gmail.com>
2022-12-20 08:09:08 -0800

    Revert "betty compliance 9-~"
    This reverts commit 8bdbc3c5270038da492e0172a3bdbeea8a7fad54.

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -9,14 +9,13 @@ int _atoi(char *s)
 {
 int sign = 1;
 unsigned int num = 0;
-do{
-if(*s == '-')
+do {
+if (*s == '-')
 sign *= -1;
-else if(*s >= '0' && *s <= '9')
+else if (*s >= '0' && *s <= '9')
 num = (num * 10) + (*s - '0');
-else if(num > 0)
+else if (num > 0)
 break;
-}
-while(*s++);
-return(num *sign);
+}while (*s++);
+return (num * sign);
 }

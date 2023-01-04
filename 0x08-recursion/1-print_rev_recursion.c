12892e8278c55fac890f468e403fe6a8198eae8f
Isokan Ogunniyi <isokandone@gmail.com>
2023-01-04 02:45:44 -0800

    1-print_rev done

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,11 +1,15 @@
 #include "main.h"
+/**
+ * _print_rev_recursion - print a string in reverse
+ * @s: string
+ * Return 0;
+ */
 
 void _print_rev_recursion(char *s)
 {
-if (*s == '\0')
-    {
-return;
-    }
+if (*s)
+{
 _print_rev_recursion(s + 1);
-putchar(*s);
+_putchar(*s);
+}
 }

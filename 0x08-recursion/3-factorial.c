91f8b8a61bb1c18e04495d5a8af30a0009c2e6ba
Isokan Ogunniyi <isokandone@gmail.com>
2023-01-04 03:45:25 -0800

    all done

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -9,17 +9,17 @@ int factorial(int n)
 int x;
 
 if (n == 0)
-    {
+{
 return (1);
-    }
+}
 else if (n < 0)
-    {
+{
 return (-1);
-    }
+}
 else
-    {
+{
 x = n * factorial(n - 1);
-    }
+}
 return (x);
 
 }

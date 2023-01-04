1276b4f40b71b7d9711629dbf47ae00d83005914
Isokan Ogunniyi <isokandone@gmail.com>
2023-01-04 02:19:49 -0800

    0-puts done

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,12 +1,19 @@
 #include "main.h"
-
+/**
+ * _puts_recursion - print a string
+ * @s:string
+ * Return:void
+ */
 void _puts_recursion(char *s)
 {
-  if (*s == '\0')
-    {
-      putchar('\n');
-      return;
-    }
-  putchar(*s);
-  _puts_recursion(s + 1);
+if (*s)
+{
+_putchar(*s);
+_puts_recursion(s + 1);
+}
+else
+{
+_putchar('\n');
+}
+
 }

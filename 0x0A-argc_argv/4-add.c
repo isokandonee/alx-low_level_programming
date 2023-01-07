bb35edc602b1254af1e733e076e79912c830cd49
Isokan Ogunniyi <isokandone@gmail.com>
2023-01-06 19:28:27 -0800

    4 done

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -22,51 +22,54 @@ int main(int argc, char **argv)
   
 {
   
-int i, n, sum = 0;
+  int i, n, sum = 0;
   
-char *flag;
+  char *flag;
   
 
   
-if (argc < 2)
+  if (argc < 2)
     
-{
+    {
       
-printf("0\n");
+      printf("0\n");
       
-return (0);
-
-}
+      return (0);
+      
+    }
   
 
   
-for (i = 1; argv[i]; i++)
+  for (i = 1; argv[i]; i++)
     
-{
+    {
       
-n = strtol(argv[i], &flag, 10);
+      n = strtol(argv[i], &flag, 10);
       
-if (*flag)
+      if (*flag)
 	
-{
-printf("Error\n");
+	{
 	  
-return (1);
-}	  
-
-else
+	  printf("Error\n");
+	  
+	  return (1);
+	  
+	}
+      
+      else
 	
-{
+	{
 	  
-sum += n;
-}
-{
+	  sum += n;
+	  
+	}
+      
+    }
   
-printf("%d\n", sum);
+  printf("%d\n", sum);
   
 
   
-return (0);
+  return (0);
   
 }
-

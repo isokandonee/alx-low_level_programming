3ae28321c58f0f8a59ec28fb217b17327578e526
Isokan Ogunniyi <isokandone@gmail.com>
2023-01-09 05:55:48 -0800

    0 task done

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -20,35 +20,36 @@
 
  */
 
-char *create_array(unsigned int size, char c) {
+char *create_array(unsigned int size, char c)
   
-  if (size == 0) {
-    
-    return NULL;
-    
-  }
+{
   
-
-  
-  char *array = malloc(sizeof(char) * size);
-  
-  if (array == NULL) {
-    
-    return NULL;
-    
-  }
+char *array;
+ 
+unsigned int i;
   
 
   
-  for (unsigned int i = 0; i < size; i++) {
+if (size == 0)
     
-    array[i] = c;
-    
-  }
+return (NULL);
   
 
   
-  return array;
+array = (char *)malloc(sizeof(char) * size);
+  
+if (array == NULL)
+    
+return (NULL);
+  
+
+  
+for (i = 0; i < size; i++)
+    
+array[i] = c;
+  
+
+  
+return (array);
   
 }
-
